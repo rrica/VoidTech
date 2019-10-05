3151f3dbe8cee9d3b719c3bbe8353e968223eecc
Dawid Rusin <dawidrusin90@gmail.com>
2019-10-05 13:30:21 +0200

    loading tilemap

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,4 +1,7 @@
 import { Scene } from 'phaser';
+import mapJson from './assets/map.json';
+import tiles from './assets/brick-sheet.png';
+import chara from './assets/textures.png'
 
 export default class GameScene extends Scene {
 	constructor() {
@@ -10,18 +13,31 @@ export default class GameScene extends Scene {
 	static get KEY() {
 		return 'game-scene'
 	}
+
+	preload() {
+		this.load.image('tiles', tiles);
+		this.load.tilemapTiledJSON('map', mapJson);
+		this.load.spritesheet('chara',
+        chara,
+        { frameWidth: 8, frameHeight: 8 }
+    );
+	}
 	
 	create() {
+		this.physics.world.TILE_BIAS = 8; // tilemap tiles are 8x8, default bias is for 16x16 and breaks collision
+		this.physics.world.OVERLAP_BIAS = 1; // we don't want to automatically resolve overlaps
+
+		const map = this.make.tilemap({ key: 'map' });
+		const tileset = map.addTilesetImage('brick-sheet', 'tiles');
+		const undestructibleLayer = map.createStaticLayer('undestructible', tileset, 0, 0);
+    	undestructibleLayer.setCollisionByProperty({ collision: true });
+
+
 		this._drops = this.physics.add.group();
-		this._player = this.physics.add.image(500, 600);
-		this._player.setInteractive();
+		const player = this.physics.add.image(4, 4, 'chara');
+		player.setInteractive();
 		
-		this.physics.add.overlap(this._player, this._drops,
-			(...args) => args.filter(arg => arg !== this._player).forEach(arg => arg.destroy())
-		);
-		
-		
-		this.input.setDraggable(this._player);
+		this.input.setDraggable(player);
 		this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
 			gameObject.x = dragX;
 			gameObject.y = dragY;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const config = {
 	type: Phaser.AUTO,
 	pixelArt: true,
 	parent: 'canvas-parent',
+	scale: {
+		parent: 'phaser',
+		mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
+		width: 64,
+		height: 64,
+		max: {
+			width: 640,
+			height: 640
+		}
+	},
 	physics: {
 		default: 'arcade',
 		arcade: { debug: true }
