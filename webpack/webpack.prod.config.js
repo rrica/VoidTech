f431af09ad3ca61232538b23d0c9d60a20d467bc
Dawid Rusin <dawidrusin90@gmail.com>
2019-10-06 20:26:58 +0200

    removed double assets from docs

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -3,17 +3,5 @@ const merge = require('webpack-merge');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(baseConfig, {
-	mode: 'production',
-	plugins: [
-		new CopyPlugin([
-			{ 
-				from: 'src/assets/Dave-talking.gif',
-				to: 'assets/Dave-talking.gif'
-			},
-			{ 
-				from: 'src/assets/Lisa-talking.gif',
-				to: 'assets/Lisa-talking.gif'
-			}	
-		]),
-	  ]
+	mode: 'production'
 });
