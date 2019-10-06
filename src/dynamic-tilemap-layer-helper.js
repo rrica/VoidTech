19a5c42ac03bfd950b429f55b3d00f928b2fd2cb
Simon Lenz <dev@simon-lenz.de>
2019-10-06 12:42:36 +0200

    add docs

--- src/dynamic-tilemap-layer-helper.js
+++ src/dynamic-tilemap-layer-helper.js
@@ -1,3 +1,7 @@
+// For some stupid reason (probably caused by me), tiled and phaser use different "tileid"s.
+// Phaser uses index that is unique over all layers and tiled uses ids that are unique in the layers.
+// So (layer 3 tile 2) has id 2 in tiled, but 57 in phaser, since there were 55 tiles in the previous layers.
+// This hacky function tries to convert tiled to phaser indices.
 export function getPhaserTileIndexFromTiledGid(layer, gid) {
     const tileIndicesOfLayer = Object.keys(layer.gidMap).map(index => parseInt(index)).sort();
     return tileIndicesOfLayer[gid];
